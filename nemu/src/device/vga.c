9833f713fec45012236d3bda5a1ec32262550ea6
Peize <blacklotuskumano@foxmail.com>
2024-11-12 20:31:43 +0800

    Finish implementing vga

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -72,13 +72,18 @@ static inline void update_screen() {
 #endif
 
 void vga_update_screen() {
-  // TODO: call `update_screen()` when the sync register is non-zero,
+  // call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
+  uint32_t is_sync = vgactl_port_base[1];
+  if (is_sync) {
+    update_screen();
+    vgactl_port_base[1] = 0;
+  }
 }
 
 void init_vga() {
   vgactl_port_base = (uint32_t *)new_space(8);
-  vgactl_port_base[0] = (screen_width() << 16) | screen_height();
+  vgactl_port_base[0] = (screen_width() << 16) | screen_height(); // width and length conbine there
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
 #else
