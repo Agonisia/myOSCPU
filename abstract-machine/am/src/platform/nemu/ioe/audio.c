93fde30cb5e8fb2630cdac6d453268d58f94965e
Peize <blacklotuskumano@foxmail.com>
2024-11-15 23:07:21 +0800

    Finish implementing sound card

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -11,16 +11,58 @@
 void __am_audio_init() {
 }
 
+/*  read the presence flag [present] and size of the stream buffer [bufsize]  */
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
+/*  initialize according to the written [freq], [channels] and [samples]  */
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+  outl(AUDIO_INIT_ADDR, 1);
 }
 
+/*  read the size of current stream buffer already in use [count] */
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
+/*  write the content of the interval [buf.start, buf.end) into the stream buffer as audio data 
+    if the free space in the stream buffer is less than the amount of audio data to be written, 
+    the write will wait until there is enough free space to write all the audio data  */
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  int audio_len = ctl->buf.end - ctl->buf.start;
+  int buffer_size = io_read(AM_AUDIO_CONFIG).bufsize;
+  int in_use_size = inl(AUDIO_COUNT_ADDR);
+  int free_space = buffer_size - in_use_size;
+
+  // not enough free space
+  while (free_space < audio_len) {
+    // maybe need some busy-wait
+    // refresh current buffer useage
+    in_use_size = inl(AUDIO_COUNT_ADDR);
+    free_space = buffer_size - in_use_size;
+  }
+
+  // use AUDIO_SBUF_ADDR as the base address of stream buffer
+  uint8_t *sbuf_start = (uint8_t *)AUDIO_SBUF_ADDR;
+  // copy the audio data from ctl->buf.start to the buffer at AUDIO_SBUF_ADDR
+  // memcpy(sbuf_start + in_use_size, ctl->buf.start, audio_len); // easy implement
+  uint8_t *buf_start = (uint8_t *)ctl->buf.start;
+  
+  // write 4 bytes at one time
+  for (int i = 0; i < audio_len / 4; i++) {
+    outl((uintptr_t)(sbuf_start + in_use_size + i * 4), *((uint32_t *)(buf_start + i * 4)));
+  }
+
+  // write remaining bytes
+  for (int i = audio_len / 4 * 4; i < audio_len; i++) {
+    outb((uintptr_t)(sbuf_start + in_use_size + i), buf_start[i]);
+  }
+
+  // update the count with the new size
+  outl(AUDIO_COUNT_ADDR, in_use_size + audio_len);
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -27,10 +27,95 @@ enum {
   nr_reg
 };
 
+enum {
+  addr_freq = reg_freq * 4,
+  addr_channels = reg_channels * 4,
+  addr_samples = reg_samples * 4,
+  addr_sbuf_size = reg_sbuf_size * 4,
+  addr_init = reg_init * 4,
+  addr_count = reg_count * 4,
+  addr_num_reg = nr_reg * 4
+};
+
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
+static int sbuf_count = 0;
+
+SDL_AudioSpec s = {};
+
+void audio_callback(void *userdata, Uint8 * stream, int len) {
+  SDL_LockAudio(); 
+  // from sbuf to SDL stream
+  SDL_memset(stream, 0, len);
+
+  // use linear buffer instead of maintaining complex ring buffer
+  if (sbuf_count > len) {
+    // data could read in sbuf larger than SDL needed
+    // move [len] bytes to SDL
+    SDL_memcpy(stream, sbuf, len); 
+    // move left data in sbuf to the front
+    SDL_memmove(sbuf, sbuf + len, sbuf_count - len); // also can use memcpy here
+    // update sbuf data count 
+    sbuf_count -= len;
+  } else {
+    // data in sbuf smaller than SDL needed
+    // move all the bytes in sbuf to SDL
+    SDL_memcpy(stream, sbuf, sbuf_count);
+    // set left part in SDL as 0
+    SDL_memset(stream + sbuf_count, 0, len - sbuf_count);
+    // set sbuf date count as 0
+    sbuf_count = 0;
+  }
+  SDL_UnlockAudio();
+}
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  SDL_LockAudio();
+  if (is_write) {
+    // write request
+    switch (offset) {
+      case addr_freq:
+        s.freq = audio_base[reg_freq];
+        break;
+      case addr_channels:
+        s.channels = audio_base[reg_channels];
+        break;
+      case addr_samples:
+        s.samples = audio_base[reg_samples];
+        break;
+      case addr_count:
+        sbuf_count = audio_base[reg_count];
+        break;
+      case addr_init:
+        if (audio_base[reg_init] == 1 && s.callback == NULL) {
+          audio_base[reg_init] = 0;
+          s.format = AUDIO_S16SYS;
+          s.userdata = NULL;
+          s.size = CONFIG_SB_SIZE;
+          s.callback = audio_callback;
+          SDL_InitSubSystem(SDL_INIT_AUDIO);
+          SDL_OpenAudio(&s, NULL);
+          SDL_PauseAudio(0);
+        }
+        break;
+      default:
+        printf("write error: offset = 0x%02x\n", offset);
+        break;
+    }
+  } else {
+    // read request
+    switch (offset) {
+      case addr_sbuf_size:
+        audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+        break;
+      case addr_count:
+        audio_base[reg_count] = sbuf_count;
+        break;
+      default:
+        break;
+    }
+  }
+  SDL_UnlockAudio();
 }
 
 void init_audio() {
